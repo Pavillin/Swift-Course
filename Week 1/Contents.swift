b8779cc386c542e46e54c6c62b99220c1080b8a7
Dylan <dylansprague11@hotmail.com>
2019-05-14 18:36:47 -0400

    Uploaded week 1 files

---
+++ Week 1/Contents.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+var str = "Hello, playground"
+var number = 10
+print("Hello Welcome to Swift")
+//normal comment
+/*
+ multi
+ line
+ comment
+ */
+
+print("\(str), this is the static part of the text") //example of string interpolation
+
+
+var number2 = 40
+var sum = number + number2
+print(number + number2)
+print(100 - 40)
+let age = 20
+var name:String = "Swift" //explicit var declaration
+var name2 = "Swift"
+
+
