703abb1a5b2a58679130bd2246857333fbf3bf3f
Hannah <Sprague@Dylans-MacBook-Air.local>
2019-06-18 22:33:06 -0300

    exercise 3 done

--- Week 5/Assignment 2/Search.playground/Contents.swift
+++ Week 5/Assignment 2/Search.playground/Contents.swift
@@ -1,11 +1,15 @@
 import Cocoa
 
-func search<T>(values: [T], value: T) -> String{
-    if(values.contains(where: T)){
-        return print("Element found")
-    }else{
-        return print("Element not found")
+var intAndDbl = [1, 2, 3, 4, 1.1, 2.2, 3.3, 4.4]
+public func Search<T : Comparable>( values: [T], valueToFind: T ) -> String {
+    for value in values {
+        if(valueToFind == value){
+            return "Element found";
+        }
     }
+    return "Element not found";
 }
 
-let dblAndInt = [1, 2, 3, 4, 5, 6, 7, 1.1, 2.2, 3.3, 4.4]
+print(Search(values: intAndDbl, valueToFind: 10));
+print(Search(values: intAndDbl, valueToFind: 1));
+print(Search(values: intAndDbl, valueToFind: 1.1));
